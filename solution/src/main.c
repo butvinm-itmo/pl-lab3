8e63fffac08cb7fc852336c16a95dcfd8988c9cb
Mihail Butvin <butvin.mihail@yandex.ru>
2023-11-18 20:08:00 +0300

    Impl read/write funcitons and main entrypoint

--- solution/src/main.c
+++ solution/src/main.c
@@ -5,24 +5,50 @@
 #include <stdlib.h>
 
 #include "bmp.h"
+#include "cmd.h"
+#include "io.h"
+#include "processing/rotation.h"
+#include "result.h"
 
-int main(int argc, char** argv) {
-    (void)argc;
-    (void)argv;  // supress 'unused parameters' warning
+IO_RESULT(read_bmp_result, from_bmp_result)
 
-    if (argc < 2) {
-        fprintf(
-            stderr,
-            "Image path missed.\nUsage: ./image-transformer <source-image> "
-            "<transformed-image> <angle>\n"
-        );
+read_bmp_result read_bmp(char *image_path) {
+    FILE *file = fopen(image_path, "r");
+    if (file == NULL) {
+        return (read_bmp_result){IO_OPEN_ERR};
     }
+    from_bmp_result result = from_bmp(file);
+    if (fclose(file) != 0) {
+        return (read_bmp_result){IO_CLOSE_ERR};
+    }
+    return (read_bmp_result){IO_OK, result};
+}
 
-    image img = {0};
-    FILE* in = fopen(argv[1], "r");
-    read_result result = from_bmp(in, &img);
-    printf("%d\n", result);
-    fclose(in);
-    printf("width: %" PRId32 " height: %" PRId32 "\n", img.width, img.height);
-    return 0;
+IO_RESULT(write_bmp_result, to_bmp_status)
+
+write_bmp_result write_bmp(char *image_path, image img) {
+    FILE *file = fopen(image_path, "w");
+    if (file == NULL) {
+        return (write_bmp_result){IO_OPEN_ERR};
+    }
+    to_bmp_status status = to_bmp(file, img);
+    if (fclose(file) != 0) {
+        return (write_bmp_result){IO_CLOSE_ERR};
+    }
+    return (write_bmp_result){IO_OK, status};
+}
+
+int main(int argc, char **argv) {
+    cmd_args_result cmd_args = parse_cmd_args(argc, argv);
+
+    read_bmp_result r_result = read_bmp(cmd_args.value.source_image_path);
+    image img = r_result.value.value;
+    image rotated_img = rotate_image(img, cmd_args.value.angle);
+    write_bmp_result w_result = write_bmp(
+        cmd_args.value.output_image_path, rotated_img);
+
+    free(img.pixels);
+    free(rotated_img.pixels);
+
+    return r_result.status || w_result.status;
 }
